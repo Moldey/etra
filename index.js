60aab61f096bcafdf6b66386f5237145173b9067
Moldey <69381703+Moldey@users.noreply.github.com>
2020-08-20 14:02:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -92,4 +92,4 @@ client.on('message', message => {
 
 
 
-client.login('NzQ2MDQ2NjA4NTc1Mjk5NjE0.Xz6oDA.2g1jCMQNOYJauL2cSeivJqgv9i4');
+client.login('NzQ2MDQ2NjA4NTc1Mjk5NjE0.Xz6oDA.o-YeaHBZk2VGlHVKo23HjJKuiA4');
